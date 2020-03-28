43be77d66813b0964ffcc51211e057773a87f727
abduelturki <abdussalam@elturki.com>
2020-03-28 19:28:24 +0000

    Started working on MMU

---
+++ memory/mmu.h
@@ -0,0 +1,33 @@
+#include<inttypes.h>
+#ifndef MMU_H
+#define MMU_H
+
+
+class MMU
+{
+public:
+    MMU();
+private:
+    uint8_t bios[0x100];
+    union Memory
+    {
+        uint8_t address[0x10000];
+        struct
+        {
+            uint8_t rom[2][0x4000];
+            uint8_t video_ram[0x2000];
+            uint8_t external_ram[0x2000];
+            uint8_t working_ram[0x2000];
+            uint8_t echo_ram[0x1E00];
+            uint8_t object_attribute_memory[0xA0];
+            uint8_t unusable_memory[0x60];
+            uint8_t io_registers[0x40];
+            uint8_t ppu[0x40];
+            uint8_t zero_page[0x80];
+            uint8_t interrupt_flag;
+        };
+    };
+
+};
+
+#endif // MMU_H
